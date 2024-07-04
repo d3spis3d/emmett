f1d26b9a6419680747a5820f8d098dee1c89bf6a
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-07-04 09:55:26 +0200

    Disabled ESDB event store e2e tests for now

--- src/packages/emmett-esdb/src/eventStore/eventstoreDBEventStore.e2e.spec.ts
+++ src/packages/emmett-esdb/src/eventStore/eventstoreDBEventStore.e2e.spec.ts
@@ -1,65 +1,65 @@
-import {
-  assertEqual,
-  collect,
-  isGlobalStreamCaughtUp,
-  streamTransformations,
-  type Event,
-} from '@event-driven-io/emmett';
-import {
-  EventStoreDBContainer,
-  StartedEventStoreDBContainer,
-} from '@event-driven-io/emmett-testcontainers';
-import { after, before, describe, it } from 'node:test';
 // import {
-//   testAggregateStream,
-//   type EventStoreFactory,
-// } from '../../../emmett/src/testing/features';
-import { getEventStoreDBEventStore } from './eventstoreDBEventStore';
+//   assertEqual,
+//   collect,
+//   isGlobalStreamCaughtUp,
+//   streamTransformations,
+//   type Event,
+// } from '@event-driven-io/emmett';
+// import {
+//   EventStoreDBContainer,
+//   StartedEventStoreDBContainer,
+// } from '@event-driven-io/emmett-testcontainers';
+// import { after, before, describe, it } from 'node:test';
+// // import {
+// //   testAggregateStream,
+// //   type EventStoreFactory,
+// // } from '../../../emmett/src/testing/features';
+// import { getEventStoreDBEventStore } from './eventstoreDBEventStore';
 
-const { stopOn } = streamTransformations;
+// const { stopOn } = streamTransformations;
 
-type MockEvent = Event<'Mocked', { mocked: true }>;
+// type MockEvent = Event<'Mocked', { mocked: true }>;
 
-void describe('EventStoreDBEventStore', () => {
-  let esdbContainer: StartedEventStoreDBContainer;
+// void describe('EventStoreDBEventStore', () => {
+//   let esdbContainer: StartedEventStoreDBContainer;
 
-  const eventStoreFactory = () => {
-    return getEventStoreDBEventStore(esdbContainer.getClient());
-  };
+//   const eventStoreFactory = () => {
+//     return getEventStoreDBEventStore(esdbContainer.getClient());
+//   };
 
-  before(async () => {
-    esdbContainer = await new EventStoreDBContainer().start();
-  });
+//   before(async () => {
+//     esdbContainer = await new EventStoreDBContainer().start();
+//   });
 
-  after(async () => {
-    await esdbContainer.stop();
-  });
+//   after(async () => {
+//     await esdbContainer.stop();
+//   });
 
-  // const teardownHook = async () => {
-  //   await esdbContainer.stop();
-  // };
+//   // const teardownHook = async () => {
+//   //   await esdbContainer.stop();
+//   // };
 
-  // await testAggregateStream(eventStoreFactory, {
-  //   getInitialIndex: () => 0n,
-  // });
+//   // await testAggregateStream(eventStoreFactory, {
+//   //   getInitialIndex: () => 0n,
+//   // });
 
-  void it('Successful subscription and processing of events', async () => {
-    const eventStore = eventStoreFactory();
-    const streamName = 'test-stream';
+//   void it('Successful subscription and processing of events', async () => {
+//     const eventStore = eventStoreFactory();
+//     const streamName = 'test-stream';
 
-    const events: MockEvent[] = [
-      { type: 'Mocked', data: { mocked: true } },
-      { type: 'Mocked', data: { mocked: true } },
-    ];
+//     const events: MockEvent[] = [
+//       { type: 'Mocked', data: { mocked: true } },
+//       { type: 'Mocked', data: { mocked: true } },
+//     ];
 
-    await eventStore.appendToStream(streamName, events);
+//     await eventStore.appendToStream(streamName, events);
 
-    const readableStream = eventStore
-      .streamEvents()
-      .pipeThrough(stopOn(isGlobalStreamCaughtUp));
+//     const readableStream = eventStore
+//       .streamEvents()
+//       .pipeThrough(stopOn(isGlobalStreamCaughtUp));
 
-    const receivedEvents = await collect(readableStream);
+//     const receivedEvents = await collect(readableStream);
 
-    assertEqual(receivedEvents.length, events.length);
-  });
-});
+//     assertEqual(receivedEvents.length, events.length);
+//   });
+// });
